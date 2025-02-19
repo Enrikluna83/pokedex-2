ae327313ae1edf36abcb4cd4c0927ad4685895a5
Enrikluna83 <naterasle2@outlook.com>
2025-02-18 21:40:38 -0600

    primer commit

---
+++ src/app/pokedex/components/PokemonList.jsx
@@ -0,0 +1,16 @@
+import PokemonCard from './PokemonCard'
+import '../styles/PokemonList.scss'
+
+function PokemonList ({ pokemons }) {
+  return (
+    <div className='pokelist'>
+      {pokemons.map(pokemon =>
+        <PokemonCard
+          key={pokemon.name}
+          url={pokemon.url}
+        />
+      )}
+    </div>
+  )
+}
+export default PokemonList
